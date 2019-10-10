872d1e0a0861e80b3cbeb22ed32d0e159c87c1a2
Jacob Rosengard <jacob.rosengard@asu.edu>
2019-10-09 20:54:20 -0700

    Initial commit of AddingMachine

---
+++ CSE 360 Assignment 2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,65 @@
+/*********************
+//Name: Jacob Rosengard
+//Student ID: 1211604456
+//Assignment #2
+ 
+ // This file contains the code for the AddingMachine class
+ // An AddingMachine object has one instance variable called "total", and has a method to return the value of  total, a method to add to total,
+ // a method to subtract from total, and a toString method which returns a string which contains a history of the changes to total
+  */
+/*********************
+ */
+
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Constructor of the AssingMachine class
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total
+	 * @return The value of the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds to the total
+	 * @param value The number to add to the total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts from the total
+	 * @param value The value to subtract from the total
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * toString method which returns a history of the values added and subtracted from the total
+	 * @return The String representation of the values added to and subtracted from the total
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Clears the value of total and resets it to value of 0
+	 */
+	public void clear() {
+	
+	}
+}
